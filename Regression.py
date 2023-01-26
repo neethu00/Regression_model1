279689a3c8b3815cf4bb9ca5c93179440b998ee6
neethu_mohanan <85782854+neethu00@users.noreply.github.com>
2023-01-26 14:36:32 +0530

    Add files via upload

---
+++ Regression.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+
+
+# In[3]:
+
+
+# load the training dataset
+
+bike_data = pd.read_csv('daily-bike-share.csv')
+bike_data['day'] = pd.DatetimeIndex(bike_data['dteday']).day
+numeric_features = ['temp', 'atemp', 'hum', 'windspeed']
+categorical_features = ['season','mnth','holiday','weekday','workingday','weathersit', 'day']
+bike_data[numeric_features + ['rentals']].describe()
+print(bike_data.head())
+
+
+
+# In[2]:
+
+
+
+
+
+# In[4]:
+
+
+# Import modules we'll need for this notebook
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# load the training dataset
+
+bike_data = pd.read_csv('daily-bike-share.csv')
+bike_data['day'] = pd.DatetimeIndex(bike_data['dteday']).day
+numeric_features = ['temp', 'atemp', 'hum', 'windspeed']
+categorical_features = ['season','mnth','holiday','weekday','workingday','weathersit', 'day']
+bike_data[numeric_features + ['rentals']].describe()
+print(bike_data.head())
+
+
+# Separate features and labels
+# After separating the dataset, we now have numpy arrays named **X** containing the features, and **y** containing the labels.
+X, y = bike_data[['season','mnth', 'holiday','weekday','workingday','weathersit','temp', 'atemp', 'hum', 'windspeed']].values, bike_data['rentals'].values
+
+# Split data 70%-30% into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+print ('Training Set: %d rows\nTest Set: %d rows' % (X_train.shape[0], X_test.shape[0]))
+
+
+# In[ ]:
+
+
+
+
